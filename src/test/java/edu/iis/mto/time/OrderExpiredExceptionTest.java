fa3e7363e8f81de77d968de27a579a2e0023fdd6
Mayar Karkout <204641@edu.p.lodz.pl>
2018-05-25 19:33:44 +0200

    Implemented order expired exception using spy mechanism

---
+++ src/test/java/edu/iis/mto/time/OrderExpiredExceptionTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.iis.mto.time;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ *
+ * @author karko
+ */
+public class OrderExpiredExceptionTest {
+
+    private OrderItem orderItem = new OrderItem();
+
+    @Test(expected = OrderExpiredException.class)
+    public void testSomeMethod() {
+        Order spiedOrder = Mockito.spy(new Order());
+        Mockito.doReturn(25).when(
+                spiedOrder).getHoursBetweenSubmitionDateAndConfirmationDate();
+
+        spiedOrder.addItem(orderItem);
+        spiedOrder.submit();
+        spiedOrder.confirm();
+    }
+}
